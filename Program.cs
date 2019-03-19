00611154434fd061df440976f5308fe7c1e423b3
Crigerprogrammer <crigerprogrammer@gmail.com>
2019-03-19 05:45:00 -0600

    Tratando nuevamente

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    //Definimos la clase escuela
+    class Escuela{
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        public string ceo;
+
+        public void Timbrar(){
+            //Metodo Timbrar
+            Console.Beep(2000,3000);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciando mi objeto
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Platzi School";
+            miEscuela.direccion = "Guatemala";
+            miEscuela.añoFundacion = 2019;
+            Console.WriteLine("TIMBRE");
+            miEscuela.Timbrar();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
